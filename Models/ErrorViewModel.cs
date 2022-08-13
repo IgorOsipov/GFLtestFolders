f2a0d28c807f3201c9444b4a5d620930171b573b
Ihor Osypov <ihor.osypov@geeksforless.com>
2022-08-13 11:08:40 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GFLtestFolders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
